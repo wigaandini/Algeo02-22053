9215b93fa4e7c6d3f51f96513ba16bc34127201f
salsbiila <salsabiilabslmh@gmail.com>
2023-11-09 19:28:36 +0700

    feat: theta variations for creating glcm

--- src/app/Salsa/texturebased.py
+++ src/app/Salsa/texturebased.py
@@ -67,7 +67,26 @@ def grayscaleImg(imgFile):
     
     return img_grayscale
 
-def createGLCM0(img):
+# def createGLCM0(img):
+#     gray_tone = []
+
+#     for i in range(len(img)):
+#         for j in range(len(img[0])):
+#             if not isElmtList(gray_tone, img[i][j]):
+#                 gray_tone.append(img[i][j])
+    
+#     glcm = [[0 for j in range(len(gray_tone))] for i in range(len(gray_tone))]
+
+#     sortList(gray_tone)
+#     gray_tone_dictionary = {gray_tone[i] : i for i in range(len(gray_tone))}
+
+#     for i in range(len(img)):
+#         for j in range(len(img)-1):
+#             glcm[gray_tone_dictionary[img[i][j]]][gray_tone_dictionary[img[i][j+1]]] += 1
+    
+#     return glcm
+
+def getGrayTone(img):
     gray_tone = []
 
     for i in range(len(img)):
@@ -80,46 +99,65 @@ def createGLCM0(img):
     sortList(gray_tone)
     gray_tone_dictionary = {gray_tone[i] : i for i in range(len(gray_tone))}
 
+    return gray_tone_dictionary
+
+def createGLCM0(img, gray_tone_dictionary, distance=1):
+    glcm = [[0 for j in range(len(gray_tone_dictionary))] for i in range(len(gray_tone_dictionary))]
+
     for i in range(len(img)):
-        for j in range(len(img)-1):
-            glcm[gray_tone_dictionary[img[i][j]]][gray_tone_dictionary[img[i][j+1]]] += 1
+        for j in range(len(img)-distance):
+            glcm[gray_tone_dictionary[img[i][j]]][gray_tone_dictionary[img[i][j+distance]]] += 1
     
     return glcm
 
-def createGLCM90(img):
-    gray_tone = []
+def createGLCM90(img, gray_tone_dictionary):
+    glcm = [[0 for j in range(len(gray_tone_dictionary))] for i in range(len(gray_tone_dictionary))]
 
-    for i in range(len(img)):
-        for j in range(len(img[0])):
-            if not isElmtList(gray_tone, img[i][j]):
-                gray_tone.append(img[i][j])
-    
-    glcm = [[0 for j in range(len(gray_tone))] for i in range(len(gray_tone))]
-
-    sortList(gray_tone)
-    gray_tone_dictionary = {gray_tone[i] : i for i in range(len(gray_tone))}
-
-    for i in range(len(img)):
-        for j in range(len(img)-1):
-            glcm[gray_tone_dictionary[img[i][j]]][gray_tone_dictionary[img[i][j+1]]] += 1
+    for j in range(len(img)):
+        for i in range(len(img)-1, 0, -1):
+            glcm[gray_tone_dictionary[img[i][j]]][gray_tone_dictionary[img[i-1][j]]] += 1
     
     return glcm
 
-def createGLCM_noSort(img):
-    gray_tone = []
+def createGLCM45(img, gray_tone_dictionary):
+    glcm = [[0 for j in range(len(gray_tone_dictionary))] for i in range(len(gray_tone_dictionary))]
 
-    for i in range(len(img)):
-        for j in range(len(img[0])):
-            if not isElmtList(gray_tone, img[i][j]):
-                gray_tone.append(img[i][j])
+    for i in range(1, len(img)):
+        tempi = i
+        j = 0
+        while tempi > 0:
+            glcm[gray_tone_dictionary[img[tempi][j]]][gray_tone_dictionary[img[tempi-1][j+1]]] += 1
+            tempi -= 1
+            j += 1
     
-    glcm = [[0 for j in range(len(gray_tone))] for i in range(len(gray_tone))]
+    for j in range(1, len(img)-1):
+        tempj = j
+        i = len(img)-1
+        while tempj < len(img)-1:
+            glcm[gray_tone_dictionary[img[i][tempj]]][gray_tone_dictionary[img[i-1][tempj+1]]] += 1
+            i -= 1
+            tempj += 1
+    
+    return glcm
 
-    gray_tone_dictionary = {gray_tone[i] : i for i in range(len(gray_tone))}
+def createGLCM135(img, gray_tone_dictionary):
+    glcm = [[0 for j in range(len(gray_tone_dictionary))] for i in range(len(gray_tone_dictionary))]
 
-    for i in range(len(img)):
-        for j in range(len(img)-1):
-            glcm[gray_tone_dictionary[img[i][j]]][gray_tone_dictionary[img[i][j+1]]] += 1
+    for i in range(1, len(img)):
+        tempi = i
+        j = len(img)-1
+        while tempi > 0:
+            glcm[gray_tone_dictionary[img[tempi][j]]][gray_tone_dictionary[img[tempi-1][j-1]]] += 1
+            tempi -= 1
+            j -= 1
+    
+    for j in range(len(img)-2, 0, -1):
+        tempj = j
+        i = len(img)-1
+        while tempj > 0:
+            glcm[gray_tone_dictionary[img[i][tempj]]][gray_tone_dictionary[img[i-1][tempj-1]]] += 1
+            i -= 1
+            tempj -= 1
     
     return glcm
 
@@ -129,6 +167,15 @@ def energy(matrix):
     for i in range(len(matrix)):
         for j in range(len(matrix[0])):
             value += (matrix[i][j])**2
+    
+    return value**(0.5)
+
+def dissimilarity(matrix):
+    value = 0
+
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            value += matrix[i][j]*abs(i-j)
     
     return value
 
@@ -151,16 +198,6 @@ def contrast(matrix):
     
     return value
 
-def variance(matrix):
-    mean = elmtSumMatrix(matrix)/(len(matrix)*len(matrix[0]))
-    value = 0
-
-    for i in range(len(matrix)):
-        for j in range(len(matrix[0])):
-            value += ((i-mean)**2)**matrix[i][j]
-    
-    return value
-
 def homogeneity(matrix):
     value = 0
 
@@ -170,20 +207,70 @@ def homogeneity(matrix):
     
     return value
 
+def mui(matrix):
+    value = 0
+
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            value += i*matrix[i][j]
+    
+    return value
+
+def muj(matrix):
+    value = 0
+
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            value += j*matrix[i][j]
+
+    return value
+
+def sigmai(matrix):
+    value = 0
+    meani = mui(matrix)
+
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            value += matrix[i][j]*((i-meani)**2)
+    
+    return value**0.5
+
+def sigmaj(matrix): #standard deviation
+    value = 0
+    meanj = muj(matrix)
+
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            value += matrix[i][j]*((j-meanj)**2)
+    
+    return value**0.5
+
+def correlation(matrix):
+    value = 0
+    meani = mui(matrix)
+    meanj = muj(matrix)
+    stdi = sigmai(matrix)
+    stdj = sigmaj(matrix)
+
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            value += matrix[i][j]*(((i-meani)*(j-meanj))/(((stdi**2)*(stdj**2))**0.5))
+
+    return value
+
 def textureBasedVector(matrix):
-    vector = [0 for i in range(5)]
+    vector = [0 for i in range(6)]
     vector[0] = contrast(matrix)
     vector[1] = homogeneity(matrix)
     vector[2] = entropy(matrix)
     vector[3] = energy(matrix)
-    vector[4] = variance(matrix)
+    vector[4] = correlation(matrix)
+    vector[5] = dissimilarity(matrix)
     
     return vector
 
-def imgFileToTextureVector(imgFile):
-    grayscaled_img = grayscaleImg(imgFile)
-    glcm = createGLCM0(grayscaled_img)
-    glcm = symmetricMatrix(glcm)
-    glcm = normalizedMatrix(glcm)
-    
-    return textureBasedVector(glcm)
+def normalizedGLCM(glcm):
+    normalized_glcm = symmetricMatrix(glcm)
+    normalized_glcm = normalizedMatrix(normalized_glcm)
+
+    return normalized_glcm
